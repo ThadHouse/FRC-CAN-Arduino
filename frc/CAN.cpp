ba55be6e9c395f8affc87a41c533466e9214b9cc
Thad House <thadhouse1@gmail.com>
2019-11-23 14:57:59 -0800

    Initial commit

---
+++ frc/CAN.cpp
@@ -0,0 +1,73 @@
+#include "frc/CAN.h"
+#include "mcp2515.h"
+
+using namespace frc;
+
+  MCP2515* CAN::m_mcp2515 = nullptr;
+  uint8_t CAN::m_interruptPin = 0;
+  CAN::MessageCallback CAN::m_messageCb = nullptr; 
+  CAN* CAN::m_canClasses[16];
+  uint8_t CAN::m_canCount = 0;
+
+
+void CAN::SetCANImpl(MCP2515* mcp, uint8_t iPin, CAN::MessageCallback cb) {
+    m_mcp2515 = mcp;
+    m_interruptPin = iPin;
+    m_messageCb = cb;
+}
+
+void CAN::Update() {
+    can_frame frame;
+    if (!digitalRead(m_interruptPin)) {
+        auto err = m_mcp2515->readMessage(&frame);
+        if (err == MCP2515::ERROR::ERROR_OK) {
+            for(uint8_t i = 0; i < m_canCount; i++) {
+                auto canClass = m_canClasses[i];
+                if (canClass == nullptr) return;
+                auto masked = frame.can_id & CAN_EFF_MASK;
+                auto matchesId = canClass->matchesId(masked);
+                if (matchesId >= 0) {
+                    m_messageCb(canClass, matchesId);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+  void CAN::AddToReadList() {
+    m_canClasses[m_canCount] = this;
+    m_canCount++;
+  }
+
+void CAN::RemoveFromReadList() {
+    for (int i = 0; i < m_canCount; i++) {
+        if (m_canClasses[i] == this) {
+            m_canClasses[i] == nullptr;
+        }
+    }
+}
+
+bool CAN::WritePacket(const uint8_t *data, uint8_t length, int apiId) {
+    can_frame frame;
+    memcpy(frame.data, data, length);
+    frame.can_dlc = length;
+    frame.can_id = m_messageMask;
+    frame.can_id |= apiId << 6;
+    frame.can_id |= CAN_EFF_FLAG;
+
+    return m_mcp2515->sendMessage(&frame) == MCP2515::ERROR::ERROR_OK;
+
+}
+
+bool CAN::WriteRTRFrame(uint8_t length, int apiId) {
+    can_frame frame;
+    memset(frame.data, 0, 8);
+    frame.can_dlc = length;
+    frame.can_id = m_messageMask;
+    frame.can_id |= apiId << 6;
+    frame.can_id |= CAN_EFF_FLAG;
+    frame.can_id |= CAN_RTR_FLAG;
+
+    return m_mcp2515->sendMessage(&frame) == MCP2515::ERROR::ERROR_OK;
+}
---
+++ frc/CAN.h
@@ -0,0 +1,94 @@
+#pragma once
+
+class MCP2515;
+
+namespace frc
+{
+struct CANData
+{
+  uint8_t data[8];
+  uint8_t length;
+  uint32_t timestamp;
+};
+
+enum class CANDeviceType
+{
+  kBroadcast = 0,
+  kRobotController = 1,
+  kMotorController = 2,
+  kRelayController = 3,
+  kGyroSensor = 4,
+  kAccelerometer = 5,
+  kUltrasonicSensor = 6,
+  kGearToothSensor = 7,
+  kPowerDistribution = 8,
+  kPneumatics = 9,
+  kMiscellaneous = 10,
+  kFirmwareUpdate = 31
+};
+
+enum class CANManufacturer
+{
+  kBroadcast = 0,
+  kNI = 1,
+  kLM = 2,
+  kDEKA = 3,
+  kCTRE = 4,
+  kREV = 5,
+  kGrapple = 6,
+  kMS = 7,
+  kTeamUse = 8,
+  kKauaiLabs = 9,
+  kCopperforge = 10,
+  kPWF = 11,
+  kStudica = 12
+};
+
+constexpr uint32_t createCANId(uint16_t apiId, uint8_t deviceId, uint8_t manufacturer, uint8_t deviceType)
+{
+  return (static_cast<uint32_t>(deviceType) & 0x1F) << 24 | (static_cast<uint32_t>(manufacturer) & 0xFF) << 16 | (apiId & 0x3FF) << 6 | (deviceId & 0x3F);
+}
+
+class CAN
+{
+public:
+  typedef void (*MessageCallback)(CAN *can, int apiId);
+  static void SetCANImpl(MCP2515 *mcp2515, uint8_t interruptPin, MessageCallback messageCallback);
+
+  static void Update();
+
+  constexpr CAN(int deviceId, CANManufacturer deviceManufacturer, CANDeviceType deviceType) 
+    : m_messageMask{createCANId(0, deviceId, static_cast<uint8_t>(deviceManufacturer), static_cast<uint8_t>(deviceType))}
+  {
+  }
+
+  constexpr explicit CAN(int deviceId) : CAN{deviceId, CANManufacturer::kTeamUse, CANDeviceType::kMiscellaneous}
+  {
+  }
+
+  void AddToReadList();
+
+  void RemoveFromReadList();
+
+  bool WritePacket(const uint8_t *data, uint8_t length, int apiId);
+
+  bool WriteRTRFrame(uint8_t length, int apiId);
+
+private:
+  static MCP2515* m_mcp2515;
+  static uint8_t m_interruptPin;
+  static CAN::MessageCallback m_messageCb; 
+  static CAN* m_canClasses[16];
+  static uint8_t m_canCount;
+
+  int16_t matchesId(int32_t id) {
+    constexpr int32_t mask = 0x1FFF003F;
+    constexpr int32_t filter = 0x0000FFC0;
+    if ((id & mask) == m_messageMask) {
+      return (id & filter) >> 6;
+    }
+    return -1;
+  }
+  uint32_t m_messageMask = 0;
+};
+} // namespace frc
